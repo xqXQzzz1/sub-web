d735fc050ef871dc405a4ad7a4b35a4cd7c11b6f
xqxzzz1 <827548000@qq.com>
2020-10-17 11:46:24 +0800

    env

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -584,7 +584,7 @@ export default {
   mounted() {
     this.form.clientType = "clash&new_name=true";
     this.form.customBackend = "https://sub-xqx.vercel.app/sub?";
-    this.form.remoteConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini";
+    this.form.remoteConfig = "https://raw.githubusercontent.com/xqXQzzz1/clashpref/main/OwnSSR_remote.ini";
     this.getBackendVersion();
   },
   methods: {
