f6fda56b802ff74ff8ab5b9af814a564cefcb2c4
xqXQzzz1 <35725489+xqXQzzz1@users.noreply.github.com>
2021-03-26 22:25:09 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -266,14 +266,14 @@ export default {
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
           "sub-xqx.vercel.app": "https://sub-xqx.vercel.app/sub?",
-          "subcon.dlj.tf(subconverter作者提供-稳定)": "https://subcon.dlj.tf/sub?",
+          "herokuapp(自用)": "https://xqxsub.herokuapp.com/sub?",
           "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
           "api.wcc.best(sub-web作者提供-稳定)": "https://api.wcc.best/sub?",
         },
         backendOptions: [
           { value: "http://localhost:25500/sub?" },
           { value: "https://sub-xqx.vercel.app/sub?" },
-          { value: "https://subcon.dlj.tf/sub?" },
+          { value: "https://xqxsub.herokuapp.com/sub?" },
           { value: "https://api.dler.io/sub?" },
           { value: "https://api.wcc.best/sub?" },
         ],
